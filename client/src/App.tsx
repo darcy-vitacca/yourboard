1dfa45d6d6d8a115b7f61ef24bdf072cbe5aa713
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-09-17 11:24:57 +1000

    updated front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,11 +7,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "../../.env" });
 
 const App = () => {
-  axios.defaults.baseURL =
-    // process.env.NODE_ENV === "production"
-    //   ?
-    "/api/";
-  // : "http://localhost:5000/api";
+  axios.defaults.baseURL = "/api/";
   axios.defaults.withCredentials = true;
   return (
     <>
