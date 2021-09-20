c329198319e5c0745eb523c502fd2abcdcb0befb
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-09-21 09:58:12 +1000

    Added entity

---
+++ server/src/entities/ProjectUser.ts
@@ -0,0 +1,36 @@
+import {
+  Entity as TOEntity,
+  Column,
+  Index,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import Entity from './Entity';
+import Project from './Project';
+
+@TOEntity('project_users')
+export default class ProjectUser extends Entity {
+  constructor(project_user: Partial<ProjectUser>) {
+    super();
+    Object.assign(this, project_user);
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index()
+  @Column({
+    nullable: true,
+  })
+  full_name: string;
+
+  @Column()
+  status: boolean;
+
+  @Column()
+  project_id: string;
+  @ManyToOne(() => Project)
+  @JoinColumn({ name: 'project_id', referencedColumnName: 'project_id' })
+  project: Project;
+}
