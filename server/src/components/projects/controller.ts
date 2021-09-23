567cac686996263e1bd3925d1327baa1dc4e9885
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-09-23 19:02:17 +1000

    merged

--- server/src/components/projects/controller.ts
+++ server/src/components/projects/controller.ts
@@ -53,7 +53,6 @@ export const createProject = async (req: Request, res: Response) => {
     });
     await project.save();
 
-
     const projectUsers = await new ProjectUser({
       full_name: `${user.firstName} ${user.lastName}`,
       status: true,
