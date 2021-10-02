dd6859933115b319cbc30f057785949a52e29c7e
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-10-02 13:54:25 +1000

    merged change

--- server/src/components/middleware/auth.ts
+++ server/src/components/middleware/auth.ts
@@ -7,7 +7,6 @@ export default async (_: Request, res: Response, next: NextFunction) => {
     const user: User | undefined = res.locals.user;
 
     if (!user) throw new Error('Unauthenticated');
-
     return next();
   } catch (err: any) {
     console.log(err);
