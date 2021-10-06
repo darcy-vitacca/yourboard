4592643a0091394110371dee803a30fff4f2f9c4
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-10-06 19:42:41 +1100

    test

--- client/src/components/context/context.tsx
+++ client/src/components/context/context.tsx
@@ -98,7 +98,6 @@ const reducer: any = (state: State, { type, payload }: Action) => {
   }
 };
 
-
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, defaultDispatch] = useReducer(reducer, {
     user: null,
