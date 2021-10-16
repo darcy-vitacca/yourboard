a97425df3a5e2dad41d1ab3ef051af7663cd6b1b
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-10-16 12:29:09 +1100

    updated styling

--- client/src/shared/Layout.styles.tsx
+++ client/src/shared/Layout.styles.tsx
@@ -50,7 +50,7 @@ export const SectionContainer = styled.div<ISectionContainerProps>`
   min-width: 280px;
   margin-left: 70px;
   @media ${device.mobileLrg} {
-    padding: 0 20px;
+    padding: 15px 20px;
     margin-left: 0;
   }
   display: ${({ align }) => (align ? "flex" : "inherit")};
