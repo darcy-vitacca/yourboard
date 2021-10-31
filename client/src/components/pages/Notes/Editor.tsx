2dbbf8585263ed31127280bf2598ff5c65067389
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2021-10-31 16:16:13 +1100

    Changed editor

--- client/src/components/pages/Notes/Editor.tsx
+++ client/src/components/pages/Notes/Editor.tsx
@@ -1,55 +1,61 @@
-import React, {  FC } from "react";
+import React, { FC } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import { Controller } from "react-hook-form";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import './Editor.css'
-import styled from 'styled-components/macro'
+import "./Editor.css";
+import styled from "styled-components/macro";
 // import { convertToRaw} from "draft-js";
 // import draftToHtml from 'draftjs-to-html';
 // import isEmpty from 'lodash/isEmpty'
 // import { Markdown } from '../../../shared/markdown';
 
-const EditorContainer = styled.div`
-`
+const EditorContainer = styled.div``;
 
 interface DraftEditorValues {
   name: string;
   defaultValue: string;
-  control: any
+  control: any;
   notes?: any;
   editorState: any;
   setEditorState: any;
 }
 
-
-export const DraftEditor: FC<DraftEditorValues> = ({name, defaultValue, control, notes, editorState, setEditorState}) => {
+export const DraftEditor: FC<DraftEditorValues> = ({
+  name,
+  defaultValue,
+  control,
+  notes,
+  editorState,
+  setEditorState,
+}) => {
   return (
     <EditorContainer>
-      <div style={{
-        minHeight: '500px',
-        color: 'black',
-        backgroundColor: '#4c5e71'
-       }}>
+      <div
+        style={{
+          minHeight: "500px",
+          color: "black",
+          backgroundColor: "#4c5e71",
+        }}
+      >
         <Controller
           name={name}
           control={control}
           defaultValue={defaultValue || ""}
           render={({ field }) => (
-        <Editor
-          {...field}
-          editorState={editorState}
-          onEditorStateChange={setEditorState}
-          wrapperClassName="wrapper-class"
-          editorClassName="editor-class"
-          toolbarClassName="toolbar-class"
-        />
+            <Editor
+              {...field}
+              editorState={editorState}
+              onEditorStateChange={setEditorState}
+              wrapperClassName="wrapper-class"
+              editorClassName="editor-class"
+              toolbarClassName="toolbar-class"
+            />
           )}
         />
       </div>
-    {/*  {!isEmpty(editorState) &&*/}
-    {/*  <Markdown children={draftToHtml(convertToRaw(editorState?.getCurrentContent()))} />*/}
-    {/*}*/}
+      {/*  {!isEmpty(editorState) &&*/}
+      {/*  <Markdown children={draftToHtml(convertToRaw(editorState?.getCurrentContent()))} />*/}
+      {/*}*/}
     </EditorContainer>
   );
-}
-
+};
